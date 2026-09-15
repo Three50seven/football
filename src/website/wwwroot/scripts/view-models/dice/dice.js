e8a13d74821521d7744f2f34310f6793f86bc607
Paul Krimm <pkrimm@nettango.com>
2026-09-15 13:16:13 -0400

    Updated dice to show rendered die faces instead of numbers only

--- src/website/wwwroot/scripts/view-models/dice/dice.js
+++ src/website/wwwroot/scripts/view-models/dice/dice.js
@@ -21,18 +21,36 @@
 
     display: function (dice, diceSum) {
         $("#diceValues").empty();
+        let pipPositions = {
+            1: [5],
+            2: [1, 9],
+            3: [1, 5, 9],
+            4: [1, 3, 7, 9],
+            5: [1, 3, 5, 7, 9],
+            6: [1, 3, 4, 6, 7, 9]
+        };
 
         for (var i = 0, len = dice.length; i < len; i++) {
             let die = dice[i];
-            let listItem = document.createElement("span");
-            listItem.className = "die-result";
-            let dieText =
-                "Die " + (i + 1) + " = " + die.valueRolled;
+            let dieFace = document.createElement("span");
+            dieFace.className = "die-result";
+            dieFace.setAttribute("role", "img");
+            dieFace.setAttribute("aria-label", "Die " + (i + 1) + " rolled " + die.valueRolled);
 
-            listItem.appendChild(document.createTextNode(dieText));
-            $("#diceTotal").text(diceSum);
-            $("#diceValues").append(listItem);
+            for (let pipPosition = 1; pipPosition <= 9; pipPosition++) {
+                let pip = document.createElement("span");
+                pip.className = "die-pip";
+
+                if (pipPositions[die.valueRolled].indexOf(pipPosition) >= 0)
+                    pip.className += " die-pip-visible";
+
+                dieFace.appendChild(pip);
+            }
+
+            $("#diceValues").append(dieFace);
         }
+
+        $("#diceTotal").text(diceSum);
 
         $("#diceValues").removeClass("dice-rolling").addClass("dice-rolling");
         window.setTimeout(function () {
